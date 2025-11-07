4d20b35f33734cf4bc4a1b7417efd1e5c4d78c8c
arthi6894 <arthi6894@gmail.com>
2025-11-07 17:23:55 +0530

    Add files via upload

---
+++ comparsions.py
@@ -0,0 +1,20 @@
+# comparsions
+
+if 5 < 4:
+    print("N")
+
+if 4 < 5:
+    print("Y")
+
+if "name" =="NAME":
+    print("O")
+
+if 10 % 2 == 0: #what does % do?
+    print("E")
+
+if 10 % 3 ==0:
+    print("I")
+
+if (4*6)==24:
+    print("S")
+
